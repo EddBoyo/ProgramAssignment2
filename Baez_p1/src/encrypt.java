10f12ba59c214a5f116c2992c2192b8025307d3a
Edd <Edd.Baez42@knights.ucf.edu>
2019-09-18 00:52:09 -0400

    testing

---
+++ Baez_p1/src/encrypt.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class encrypt {
+
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		int[] userDigits = new int[4];
+		System.out.println("Enter the 1st digit");
+	}
+
+}
